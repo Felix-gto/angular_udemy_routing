d68c2b13fa3ef0448e052c3868d5aad5eb87d201
Felix <felix.daraban@gmail.com>
2022-02-17 17:14:45 +0200

    Added Login / Logout buttons to use the Fake Auth Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -17,6 +18,16 @@ export class HomeComponent implements OnInit {
 
     // Example: if we want to navigate to a page after clicking a button which does complex calculations
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  // Sets loggedIn to true
+  onLogin() {
+    this.authService.login();
+  }
+
+  // Sets loggedIn to false
+  onLogout() {
+    this.authService.logout();    
   }
 
 }
